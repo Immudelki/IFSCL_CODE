8c4dce776a76b7c8e764fc61c89ddd37656498c3
Immudelki <contact@alexisfoletto.com>
2024-06-12 15:14:07 +0200

    Update CreatureUnique.cs
    2024 edition

--- CreatureUnique.cs
+++ CreatureUnique.cs
@@ -1,36 +1,68 @@
-﻿using UnityEngine;
 using System.Collections.Generic;
-using System;
-public class CreatureUnique : MonoBehaviour{		
-	public bool created=false;
-    public DateTime minimumDateTime_TillReuse;
-	public string nom="SANSnom";
-    public int hoursBeforeReuse = 6;
-	public static List<CreatureUnique>liste=new List<CreatureUnique>();
-	public CreatureUnique() {
-	}
-    public virtual void OnVirt() {
-        created = true;
+using IFSCL.Save;
+using Sirenix.OdinInspector;
+using UnityEngine;
+namespace IFSCL.VirtualWorld {
+    public class CreatureUnique : MonoBehaviour {
+        public bool created;
+        [SerializeField] public TimeUnitDateManager time_TillReuse = new TimeUnitDateManager();
+        public string nom = "unknown";
+        public static List<CreatureUnique> liste = new List<CreatureUnique>();
+        public void Awake() {
+            liste.Add(this);
+        }
+        public virtual void Init() {
+        }
+        public static void RAZ_FromRestart() {
+            liste.Clear();
+        }
+        public virtual void OnVirt() {
+            if (created) {
+                MSG.AffCriticalInfo("unique creature " + nom + " already created, a duplicate spawn will cause issues");
+            }
+            created = true;
+        }
+        [Button]
+        public virtual void RazClassique() {
+            LyokoGuide.GetByName(nom)?.Devirtualiser();
+        }
+        [Button]
+        public virtual void RazMinimum() {
+            if (!created)
+                return;
+            created = false;
+            if (GetType() == typeof(GardienFonctions)) {
+                DebugLogList.LogUniqueCreature(nom,"SetTimeTillReuse: 24",this.gameObject);
+                time_TillReuse.Set(24, AgendaTimeMarkerType.guardianCanBeReused);
+            } else if (GetType() == typeof(XanafiedFonctions)) {
+                DebugLogList.LogUniqueCreature(nom,"SetTimeTillReuse: 24",this.gameObject);
+                time_TillReuse.Set(24, AgendaTimeMarkerType.lwXanafiedReuse, nom);
+            } else if (GetType() == typeof(MeduseFonctions)) {
+                DebugLogList.LogUniqueCreature(nom,"SetTimeTillReuse: 12",this.gameObject);
+                time_TillReuse.Set(12, AgendaTimeMarkerType.scyphozoaCanBeReused, nom);
+            } else {
+                DebugLogList.LogUniqueCreature(nom,"SetTimeTillReuse: 24",this.gameObject);
+                time_TillReuse.Set(24);
+            }
+        }
+        [Button]
+        public virtual bool CanSpawn() {
+            return !time_TillReuse.IsRUNNING();
+        }
+        public void RazCompteur() {
+            time_TillReuse.DESACTIVER();
+        }
+        public static void Load_FromSavegame(List<TimeUnitDateSubSaveData> timeUniqueCreatures) {
+            if (timeUniqueCreatures == null)
+                return;
+            int index = 0;
+            foreach (CreatureUnique cu in liste) {
+                if (index < timeUniqueCreatures.Count) {
+                    cu.time_TillReuse.LoadFrom_Savegame(timeUniqueCreatures[index]);
+                    DebugLogList.LogUniqueCreature(cu.nom,"SetTimeTillReuse from save",cu.gameObject);
+                    index++;
+                }
+            }
+        }
     }
-    public virtual bool CouldSpawn() {
-        return false;
-    }
-    public void razClassique(){
-		if (LyokoGuide.IdentifierGuide(nom)!=999)
-			LyokoGuide.GetByID(LyokoGuide.IdentifierGuide(nom)).Devirtualiser();
-	}
-	public virtual void razMinimum(){
-		created=false;
-        minimumDateTime_TillReuse = VarG.fictionalTime.AddHours(hoursBeforeReuse);
-	}
-    public bool CanBeUsed() {
-        return GetSpanTime_TillNextReuse().CompareTo(TimeSpan.Zero) <= 0;
-    }
-    public System.TimeSpan GetSpanTime_TillNextReuse() {
-        System.TimeSpan tt = minimumDateTime_TillReuse - VarG.fictionalTime;
-        return tt;
-    }
-	public void razCompteur(){
-        minimumDateTime_TillReuse = VarG.fictionalTime;
-	}
 }
