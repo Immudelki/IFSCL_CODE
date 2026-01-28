654062bd1a1aef1e7f823aa4ff27f208253a8551
Immudelki <alexis.foletto@orange.fr>
2026-01-28 10:26:39 +0100

    Update FCarthageSensorsPrefab.cs to IFSCL 48X (2026)

--- FCarthageSensorsPrefab.cs
+++ FCarthageSensorsPrefab.cs
@@ -1,20 +1,22 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 namespace IFSCL.Programs {
     public class FCarthageSensorsPrefab : FContentPrefab {
         public RawImage rawImage;
         public Animator animator;
-        public Text energyValues;
-        public TextMeshProUGUI zoomValue;
-        public TextMeshProUGUI mazeField;
-        public TextMeshProUGUI keyInfo;
-        public Color keyNotFoundColor;
-        public Color keyFoundColor;
-        public Color mazeClosingColor;
+        public TextMeshProUGUI zoomValue, mazeField, keyInfo;
+        public Color keyNotFoundColor, keyFoundColor, mazeClosingColor;
         public GameObject rebootPanel;
+        public ArrowsMapDisplay arrowsMapDisplay;
+        public MousePressDetector mousePressDetector;
         public void DoCameraAction(int ID) { //onclick
             ProgramsF.GetAByOS<PrgCarthageSensors>(OSTarget.SC).OnClickBtn_DoCameraAction(ID);
+        }
+        public void Update() {
+            if (RAZ.isRestarting)
+                return;
+            arrowsMapDisplay.DoUpdate(logicAction.GetCam3D().rtsCam, logicAction.GetCam3D().rtsCamMouse.enabled && mousePressDetector.mousePressed);
         }
     }
 }
